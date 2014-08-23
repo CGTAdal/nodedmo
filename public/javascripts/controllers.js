e738ada663f5202fb20a2a1613f5200051f5709a
Adal Singh <adal@cgt.co.in>
2014-08-23 18:27:39 +0530

    completely converted in postgresql and handled deleted entry while someone entring or viewing vote.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -40,26 +40,49 @@ function PollListCtrl($scope, socket, Poll) {
   };
 }
 // Voting / viewing poll results
-function PollItemCtrl($scope, $routeParams, socket, Poll) { 
+function PollItemCtrl($scope, $location, $routeParams, socket, Poll) { 
   // $scope.poll = Poll.get({pollId: $routeParams.pollId});
   // $scope.vote = function() {};
 
   $scope.poll = Poll.get({pollId: $routeParams.pollId});
+  // console.log($scope.poll);
   socket.on('newadded:vote', function(data) {
-    console.dir(data);
-    if(data.id === $routeParams.pollId) {
-      $scope.poll = data;
+    // console.log(data.id+'='+$routeParams.pollId);
+    if(data.id == $routeParams.pollId) {
+      $scope.poll.id = data.id;
+      $scope.poll.question = data.question;
+      $scope.poll.status = data.status;
+      $scope.poll.choices = data.choices;
+      $scope.poll.userVoted = data.userVoted;
+      $scope.poll.userChoice = data.userChoice;
+      $scope.poll.totalVotes = data.totalVotes;
     }
   });
+
   socket.on('broadcast:vote', function(data) {
-    console.dir(data);
-    if(data.id === $routeParams.pollId) {
+    // console.log(data);
+    if(data.id == $routeParams.pollId) {
+      // $scope.poll.choices = data.choices;
+      // $scope.poll.totalVotes = data.totalVotes;
+      $scope.poll.id = data.id;
+      $scope.poll.question = data.question;
+      $scope.poll.status = data.status;
       $scope.poll.choices = data.choices;
+      $scope.poll.userVoted = data.userVoted;
+      $scope.poll.userChoice = data.userChoice;
       $scope.poll.totalVotes = data.totalVotes;
     }   
   });
+
+  socket.on('broadcast_remove:questions', function(data) {
+    // console.dir(data);
+    if(data.id == $routeParams.pollId) {
+      $location.path('polls');
+    }
+  });
+
   $scope.vote = function() {
-    var pollId = $scope.poll._id,
+    var pollId = $scope.poll.id,
         choiceId = $scope.poll.userVote;
     if(choiceId) {
       var voteObj = { poll_id: pollId, choice: choiceId };
@@ -71,10 +94,18 @@ function PollItemCtrl($scope, $routeParams, socket, Poll) {
 }
 // Creating a new poll
 function PollNewCtrl($scope, $location, socket, Poll) {
+  // $scope.poll = {
+  //   question: '',
+  //   choices: [ { text: '' }, { text: '' }, { text: '' }, { text: '' }]
+  // };
   $scope.poll = {
     question: '',
-    choices: [ { text: '' }, { text: '' }, { text: '' }, { text: '' }]
-  };  
+    option_1: '',
+    option_2: '',
+    option_3: '',
+    option_4: ''
+  };
+
   $scope.addChoice = function() {
     $scope.poll.choices.push({ text: '' });
   };
@@ -82,12 +113,19 @@ function PollNewCtrl($scope, $location, socket, Poll) {
     var poll = $scope.poll;
     if(poll.question.length > 0) {
       var choiceCount = 0;
-      for(var i = 0, ln = poll.choices.length; i < ln; i++) {
-        var choice = poll.choices[i];        
-        if(choice.text.length > 0) {
+      // for(var i = 0, ln = poll.choices.length; i < ln; i++) {
+      //   var choice = poll.choices[i];        
+      //   if(choice.text.length > 0) {
+      //     choiceCount++
+      //   }
+      // }
+      for(var i = 1, ln = 4; i <= ln; i++) {
+        var choice = eval('poll.option_'+[i]);
+        if(choice.length > 0) {
           choiceCount++
         }
-      }    
+      }  
+      
       if(choiceCount > 1) {
         // var newPoll = new Poll(poll);
         // newPoll.$save(function(p, resp) {
--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ module.exports.list = function(req, res) {
 // JSON API for getting a single poll
 module.exports.poll = function(req, res) {  
   var pollId = req.params.id;
-  Poll.findById(pollId, '', { lean: true }, function(err, poll) {
+  /*Poll.findById(pollId, '', { lean: true }, function(err, poll) {
     if(poll) {
       var userVoted = false,
           userChoice,
@@ -57,6 +57,79 @@ module.exports.poll = function(req, res) {
     } else {
       res.json({error:true});
     }
+  });*/
+
+  pg.connect(conString, function(err, client, done) {
+    if(err) {
+      return console.error('error fetching client from pool', err);
+    }
+    
+    client.query("SELECT polls.id AS pollid,polls.question,polls.option_1,polls.option_2,polls.option_3,polls.option_4,polls.status,votes.* FROM polls LEFT JOIN votes ON polls.id=votes.poll_id WHERE status='1' AND polls.id=$1",[pollId], function(err, result) {
+      //call `done()` to release the client back to the pool
+      done();
+
+      if(err) {
+        // return console.error('error running query', err);
+        console.error('error running query', err);
+        // throw 'Error';
+        res.json({error:true});
+      }else{
+        // console.log(result.rows[0].number);
+        var userVoted = false, userChoice, totalVotes = 0;
+        var request_ip = (req.header('x-forwarded-for') || req.ip);
+        var choices = [{id:1,text:'',votes:0},{id:2,text:'',votes:0},{id:3,text:'',votes:0},{id:4,text:'',votes:0}];
+        totalVotes = result.rows.length;
+        
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows[i];
+          // console.log(row.ip+'='+request_ip);
+          if(row.ip === request_ip) {
+            userVoted = true;
+            userChoice = { id: row.option_value, text: eval('row.option_'+row.option_value) };
+          }
+        }
+
+        client.query("SELECT COUNT(votes.id) AS total_val,votes.option_value FROM polls LEFT JOIN votes ON polls.id=votes.poll_id WHERE status='1' AND polls.id=$1 GROUP BY votes.option_value",[pollId], function(err, result2) {
+          done();
+          if(err) {
+            // return console.error('error running query', err);
+            console.error('error running query', err);
+            // throw 'Error';
+            res.json({error:true});
+          }else{
+            var op1,op2,op3,op4 = 0;
+            
+            for (var i = 0; i < result2.rows.length; i++) {
+              var row = result2.rows[i];
+              switch(row.option_value){
+                case 1 : op1 = row.total_val; break;
+                case 2 : op2 = row.total_val; break;
+                case 3 : op3 = row.total_val; break;
+                case 4 : op4 = row.total_val; break;
+              }
+            }
+            choices = [{id:1,text:'',votes:op1},{id:2,text:'',votes:op2},{id:3,text:'',votes:op3},{id:4,text:'',votes:op4}];
+
+            var poll = {};
+            poll.id = result.rows[0].pollid;
+            poll.question = result.rows[0].question;
+            poll.status = result.rows[0].status;
+
+            for(var index=0;index<4;index++){
+              choices[index].text = eval('result.rows[0].option_'+(index+1));
+            }
+
+            poll.choices = choices;
+            poll.userVoted = userVoted;
+            poll.userChoice = userChoice;
+            poll.totalVotes = totalVotes;
+            
+            res.json(poll);
+          }
+        });
+        // res.json(result.rows[0]);
+      }
+    });
   });
 };
 // JSON API for creating a new poll
@@ -78,8 +151,9 @@ module.exports.poll = function(req, res) {
 exports.vote = function(socket) {
   // TO HANDLE THE REAL TIME VOTTING
   socket.on('send:vote', function(data) {
-    var ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;    
-    Poll.findById(data.poll_id, function(err, poll) {
+    // var ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address.address;
+    var ip = socket.request.connection.remoteAddress || socket.handshake.address.address;
+    /*Poll.findById(data.poll_id, function(err, poll) {
       var choice = poll.choices.id(data.choice);
       choice.votes.push({ ip: ip });
       poll.save(function(err, doc) {
@@ -102,6 +176,82 @@ exports.vote = function(socket) {
         socket.emit('newadded:vote', theDoc);
         socket.broadcast.emit('broadcast:vote', theDoc);
       });     
+    });*/
+    pg.connect(conString, function(err, client, done) {
+      if(err) {
+        return console.error('error fetching client from pool', err);
+      }
+      var datetime = '2014-08-23 15:40:00';
+      client.query("INSERT INTO votes (poll_id,option_value,created_on,ip) VALUES ($1,$2,'"+datetime+"',$3) RETURNING *", [data.poll_id,data.choice,ip], function(err, voteresult) {
+        // console.log(voteresult); console.log(ip);
+        client.query("SELECT polls.id AS pollid,polls.question,polls.option_1,polls.option_2,polls.option_3,polls.option_4,polls.status,votes.* FROM polls LEFT JOIN votes ON polls.id=votes.poll_id WHERE status='1' AND polls.id=$1",[data.poll_id], function(err, result) {
+          //call `done()` to release the client back to the pool
+          done();
+
+          if(err) {
+            // return console.error('error running query', err);
+            console.error('error running query', err);
+            // throw 'Error';
+            res.json({error:true});
+          }else{
+            // console.log(result.rows[0].number);
+            var userVoted = false, userChoice, totalVotes = 0;
+            // var request_ip = ip;
+            var choices = [{id:1,text:'',votes:0},{id:2,text:'',votes:0},{id:3,text:'',votes:0},{id:4,text:'',votes:0}];
+            totalVotes = result.rows.length;
+            
+            for (var i = 0; i < result.rows.length; i++) {
+              var row = result.rows[i];
+
+              if(row.ip === ip) {
+                userVoted = true;
+                userChoice = { id: row.option_value, text: eval('row.option_'+row.option_value) };
+              }
+            }
+
+            client.query("SELECT COUNT(votes.id) AS total_val,votes.option_value FROM polls LEFT JOIN votes ON polls.id=votes.poll_id WHERE status='1' AND polls.id=$1 GROUP BY votes.option_value",[data.poll_id], function(err, result2) {
+              done();
+              if(err) {
+                // return console.error('error running query', err);
+                console.error('error running query', err);
+                // throw 'Error';
+                res.json({error:true});
+              }else{
+                var op1,op2,op3,op4 = 0;
+                for (var i = 0; i < result2.rows.length; i++) {
+                  var row = result2.rows[i];
+                  switch(row.option_value){
+                    case 1 : op1 = (row.total_val)? row.total_val : 0; break;
+                    case 2 : op2 = (row.total_val)? row.total_val : 0; break;
+                    case 3 : op3 = (row.total_val)? row.total_val : 0; break;
+                    case 4 : op4 = (row.total_val)? row.total_val : 0; break;
+                  }
+                }
+                choices = [{id:1,text:'',votes:op1},{id:2,text:'',votes:op2},{id:3,text:'',votes:op3},{id:4,text:'',votes:op4}];
+
+                var poll = {};
+                // console.log(result.rows[0]);
+                poll.id = result.rows[0].pollid;
+                poll.question = result.rows[0].question;
+                poll.status = result.rows[0].status;
+
+                for(var index=0;index<4;index++){
+                  choices[index].text = eval('result.rows[0].option_'+(index+1));
+                }
+
+                poll.choices = choices;
+                poll.userVoted = userVoted;
+                poll.userChoice = userChoice;
+                poll.totalVotes = totalVotes;
+                // console.log(poll);
+                socket.emit('newadded:vote', poll);
+                socket.broadcast.emit('broadcast:vote', poll);
+              }
+            });
+            // res.json(result.rows[0]);
+          }
+        });
+      });
     });
   });
   // TO HANDLE THE REAL TIME QUESTION ADDITION
@@ -124,7 +274,7 @@ exports.vote = function(socket) {
       }
       // var datetime = new Date();
       var datetime = "2014-08-22 18:38:00";
-      client.query("INSERT INTO polls (question,status,created_on,option_1,option_2,option_3,option_4) VALUES ($1,true,'"+datetime+"',$2,$3,$4,$5) RETURNING *", [pollObj.question,pollObj.choices[0].text,pollObj.choices[1].text,pollObj.choices[2].text,pollObj.choices[3].text], function(err, result) {
+      client.query("INSERT INTO polls (question,status,created_on,option_1,option_2,option_3,option_4) VALUES ($1,true,'"+datetime+"',$2,$3,$4,$5) RETURNING *", [pollObj.question,pollObj.option_1,pollObj.option_2,pollObj.option_3,pollObj.option_4], function(err, result) {
         //call `done()` to release the client back to the pool
         done();
         if(err) {
@@ -166,7 +316,7 @@ exports.vote = function(socket) {
           console.error('error running query', err);
           throw 'Error';
         }else{
-          console.log(result);
+          // console.log(result);
           socket.broadcast.emit('broadcast_remove:questions', result.rows[0]);
         }
       });
